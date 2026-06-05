0e7bc73b98d6f493f5aa644d1f788aa0ed84daac
VigneshMP21 <mpvignesh06@gmail.com>
2026-06-05 13:20:37 +0530

    Initial Bot

---
+++ index.js
@@ -0,0 +1,19 @@
+const { Telegraf } = require("telegraf");
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start((ctx) => {
+  ctx.reply(
+    "Hi 👋 Welcome to CAI Class Portal"
+  );
+});
+
+bot.hears("Hi", (ctx) => {
+  ctx.reply(
+    "Hello 👋 How can I help you?"
+  );
+});
+
+bot.launch();
+
+console.log("Bot Started...");
